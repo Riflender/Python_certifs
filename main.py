4e5c8d9e375f44f48c5dbc23370a458725c5735f
Riflender <remilehenaff@gmail.com>
2023-02-11 15:58:55 +0100

    test multithreading

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from multiprocessing import cpu_count
-from os import listdir
+from os import listdir, cpu_count
 from requests import get
 import ssl
+import threading
 
 from time import time
+
+
+def get_cert(url: str, cert_list: list, error_list: list, cert_lock: threading.Lock, error_lock: threading.Lock):
+    try:
+        cert = ssl.PEM_cert_to_DER_cert(ssl.get_server_certificate((url, 443), timeout=5))
+        with cert_lock:
+            cert_list.append(cert)
+    except Exception as e:
+        with error_lock:
+            error_list.append((url, e))
+
 
 MAX_CPU = cpu_count()
 
@@ -28,16 +39,26 @@ error_list = []
 
 a = time()
 
+# cert_locks = [threading.Lock() for i in range(8)]
+cert_lock = threading.Lock()
+error_lock = threading.Lock()
+threads = [threading.Thread(target=get_cert, args=(url, cert_list, error_list, cert_lock, error_lock), name=f"th{i % 8}") for i, url in enumerate(part_list)]
+while threads:
+    for i in range(min(len(threads), 8)):
+        thread = threads.pop(0)
+        thread.start()
+        thread.join()
+
+"""
 for url in part_list:
     try:
-        pem = ssl.get_server_certificate((url, 443), timeout=5)
-        der = ssl.PEM_cert_to_DER_cert(pem)
-        cert_list.append(der)
+        cert_list.append(ssl.PEM_cert_to_DER_cert(ssl.get_server_certificate((url, 443), timeout=5)))
     except Exception as e:
         error_list.append((url, e))
+"""
 
 print(f"{time() - a:.2f}")
 
-print(f"{error_list.__len__()} erreurs sur {url_list.__len__():_} URLs")
+print(f"{len(error_list)} erreurs sur {len(url_list):_} URLs")
 
 print()
