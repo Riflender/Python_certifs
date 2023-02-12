769739aa3f472c240a0e10c6a92f5af703fc2c57
Riflender <remilehenaff@gmail.com>
2023-02-12 13:28:24 +0100

    add lenght and algorithm info to Certificate

--- Certificate.py
+++ Certificate.py
@@ -1,4 +1,7 @@
-from ssl import get_server_certificate, PEM_cert_to_DER_cert
+from ssl import get_server_certificate
+import OpenSSL
+from cryptography import x509
+from time import time
 
 
 class Certificate:
@@ -6,11 +9,16 @@ class Certificate:
     def __init__(self, url):
         self.url = url
         self.PEM = None
-        self.DER = None
+        self.cert = None
+        self.algorithm = None
         self.error = None
 
         try:
             self.PEM = get_server_certificate((url, 443), timeout=5)
-            self.DER = PEM_cert_to_DER_cert(self.PEM)
+            self.cert = x509.load_pem_x509_certificate(self.PEM.encode())
+            self.algorithm = self.cert.signature_algorithm_oid._name
         except Exception as e:
             self.error = e
+
+    def __len__(self):
+        return len(self.PEM)
